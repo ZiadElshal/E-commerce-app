1b246357f9609a1ba821d3b7cd92d2639436505a
ZiadElshal <zezoelzamalkawy14@gmail.com>
2024-08-30 20:29:48 +0300

    Added basics.

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/core/my_block_observer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main(){
+  Bloc.observer = MyBlocObserver();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: child,
+      ),
+    );
+  }
+}
